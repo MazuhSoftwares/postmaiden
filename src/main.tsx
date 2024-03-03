c406dc41a962d0d8192ad2fb93c270453b71a0a6
mazuh <marcell-mz@hotmail.com>
2024-03-03 00:30:46 -0300

    firebase analytics: page view
    never used an analytics by myself before... so just seeing how it works.
    
    (relax, its just views count, i dont care about your data, really.)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme/ThemeProvider.tsx";
@@ -11,3 +13,19 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+if (import.meta.env.PROD) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCvk30vFQMc8LgonvQgEdif_2XvQ_jsEmU",
+    authDomain: "postmaiden.firebaseapp.com",
+    projectId: "postmaiden",
+    storageBucket: "postmaiden.appspot.com",
+    messagingSenderId: "949749540150",
+    appId: "1:949749540150:web:ba75fbb4380fdf60a2bf97",
+    measurementId: "G-3H0D6YRC39",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  logEvent(analytics, "page_view");
+}
